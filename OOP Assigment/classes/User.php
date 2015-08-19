59f14e8f8bc9549a05fc5dd7e7b6a099b44641b8
amina <amina.nisar@coeus-solutions-de>
2015-08-20 01:06:44 +0500

    OOP

---
+++ OOP Assigment/classes/User.php
@@ -0,0 +1,27 @@
+
+<?php 
+//
+
+class User
+{
+      public $u_id;
+      public $name;
+      public $phone_no;
+      public $email="cc";
+      public $password;
+      public $tweets=0;
+
+
+/*
+      public function __construct($u, $n, $ph, $e, $p)
+      {
+      	$this->u_id= $u; 
+      	$this->name= $n;
+      	$this->phone_no= $ph;
+      	$this->email= $e;
+      	$this->password= $p;
+      } */
+	  
+}
+
+?>
